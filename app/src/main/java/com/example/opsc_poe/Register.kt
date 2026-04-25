158b479a5aeb8ac890e9b24a645327f33c50c6fc
KabeloSek <ST10444551@rcconnect.edu.za>
2026-04-25 13:46:26 +0200

    Layouts for Login and Register updated

--- app/src/main/java/com/example/opsc_poe/Register.kt
+++ app/src/main/java/com/example/opsc_poe/Register.kt
@@ -1,6 +1,7 @@
 package com.example.opsc_poe
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class Register : AppCompatActivity() {
             insets
         }
     }
+
+    fun NavigateToLogin(view: View) {}
 }
